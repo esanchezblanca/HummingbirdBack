6ddda3de87ae99399aaf1f83f399fa6955319d3e
esanchezblanca <ele.sp@hotmail.com>
2021-01-17 02:13:15 +0100

    comentarios añadidos

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const UserModel = require('./models/users');
 
-//Introducimos el nombre de la base de datos
+//Introducimos el nombre de la base de datos, user y password
 const sequelize = new Sequelize('ZxmMUcxNou', 'ZxmMUcxNou', 'Er7l3NlgbS', {
     host: 'remotemysql.com',
     dialect: 'mysql',
@@ -10,6 +10,7 @@ const sequelize = new Sequelize('ZxmMUcxNou', 'ZxmMUcxNou', 'Er7l3NlgbS', {
 
 const User = UserModel(sequelize, Sequelize);
 
+//Cuando migre la bbdd devolverá el mensaje de que ya lo ha hecho
 sequelize.sync({ force: false})
 .then(() => {
     console.log('Sincronizado')
--- index.js
+++ index.js
@@ -8,12 +8,14 @@ require('./db');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 //Respuesta de la app en el localhost si funciona correctamente
 app.get('/', (req, res) => {
     res.send('App is working');
 });
 
 
+//Determinamos en qué puerto se levanta
 app.listen(3000,() => {
     console.log('Server levantado')
 });
