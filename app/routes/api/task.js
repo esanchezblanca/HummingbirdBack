796b3ce3666bb35b7f8ac4df484f1319315f1abe
esanchezblanca <ele.sp@hotmail.com>
2021-01-19 20:42:54 +0100

    comprobación de user_id al modificar tarea

--- app/routes/api/task.js
+++ app/routes/api/task.js
@@ -1,32 +1,38 @@
 const router = require('express').Router();
-
 const { Task } = require('../../../db');
 
 
 //Mostrar todas las tareas
+
 router.get('/', async (req, res) =>{
     const task = await Task.findAll();
     res.json(task);
 });
 
+
 //Insertar nueva tarea
+
 router.post('/', async (req, res) =>{
     const task = await Task.create(req.body);
     res.json(task);
 });
 
+
 //Modificar tarea
+//Se comprueba que el id de usuario que quiere modificar coincide con quien lo creó
+
 router.put('/:taskId', async (req, res) =>{
     const task = await Task.update(req.body, {
         where: { id: req.params.taskId, user_id: req.body.user_id }
     });
     if(task == 0){
-        res.json({message: 'Error al modificar la tarea', status: 401})
+        res.json({message: 'No tienes permisos para modificar la tarea', status: 401})
     }else{
         res.json({ success: 'Tarea modificada', status: 200})
     }
     
 });
+
 
 //Eliminar tarea
 
