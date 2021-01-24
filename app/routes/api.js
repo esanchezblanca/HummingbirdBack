06785dc4a4922a0faa3bf5a8591b0c7d28c1bc33
esanchezblanca <ele.sp@hotmail.com>
2021-01-24 18:48:21 +0100

    subida a heroku modificada bbdd

--- app/routes/api.js
+++ app/routes/api.js
@@ -10,7 +10,7 @@ const apiCommentRouter = require('./api/comment');
 //Todas las rutas de task pasan antes por el middleware
 router.use('/user', apiUsersRouter);
 router.use('/task', middleware.checkToken, apiTaskRouter);
-router.use('/comment', apiCommentRouter);
+router.use('/comment', middleware.checkToken, apiCommentRouter);
 
 
 module.exports = router;
